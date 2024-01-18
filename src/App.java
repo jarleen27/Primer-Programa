2dfa87a72f23ccc357378815ad7be1d7f7d13f58
jarleen <jarleenmartinez@hotmail.com>
2024-01-18 19:13:07 -0400

    primer commit

---
+++ src/App.java
@@ -0,0 +1,7 @@
+public class App {
+    public static void main(String[] args) {
+        String name = "rosita";
+        double monto = 475.50;
+        double total = monto * 1.15;
+        System.out.println("HOLI " + name + " El total de su pago es de: " + total) ;}
+}
